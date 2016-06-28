eaf65dfafb72b974eac6d9bad44d36e705320261
saumya <saumya@pivotaldesign.biz>
2016-06-28 18:02:14 +0530

    UI for confirmation screen

--- HZTB/VCRegistrationConfirmation.swift
+++ HZTB/VCRegistrationConfirmation.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class VCRegistrationConfirmation: UIViewController {
+    
+    @IBOutlet var bg:UIImageView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //
         print("VCRegistrationConfirmation : viewDidLoad : ")
+        
+        
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        print("VCRegistrationConfirmation : viewWillAppear : ")
+    }
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        print("VCRegistrationConfirmation : viewDidAppear : ")
+        
+        let url = NSURL(string: "http://lorempixel.com/g/40/620")
+        if let data = NSData(contentsOfURL: url!) {
+            bg!.image = UIImage(data: data)
+        }
+        
+        //bg?.image = UIImage(data: "http://lorempixel.com/g/400/200")
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
